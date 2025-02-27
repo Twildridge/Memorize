2fe72b46ed2c347be8109b6bf2f680213cf7a0e0
Twildridge <semiinfiniteslab@icloud.com>
2025-02-26 23:57:58 -0800

    array-ified the cards with ForEach

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,13 +7,15 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    let emojis = ["🗿","🍆","🍑","💦"]
+    
     var body: some View {
         HStack{
-            CardView(isFaceUp: true)
-            CardView()
-            CardView(isFaceUp: true)
-            CardView()
+            ForEach(emojis.indices,id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
         .foregroundColor(.pink)
         .imageScale(.small)
@@ -21,7 +23,9 @@ struct ContentView: View {
     }
 }
 
+
 struct CardView: View {
+    let content: String
     @State var isFaceUp = false
     
     var body: some View {
@@ -30,7 +34,7 @@ struct CardView: View {
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 5)
-                Text("💝").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
